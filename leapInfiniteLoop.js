b9309827226875d548c8c27ca2f2eb837b96ed6b
racheleyg12 <rgoldma1@uvm.edu>
2020-09-07 19:22:25 -0400

    del 1 step51

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -1,15 +1,13 @@
 var controllerOptions = {};
-var i = 0;
 var x = window.innerWidth/2.0;
 var y = window.innerHeight/2.0; 
 Leap.loop(controllerOptions, function(frame){
 	clear();
-	console.log(i); 
-	i=i+1;
 	//Create a random integer between -1 and +1
 	var max = 1;
 	var min = -1;
-	var randInt = Math.floor(Math.random() * (max - min + 1) ) + min;
+	var randIntX = Math.floor(Math.random() * (max - min + 1) ) + min;
+	var randIntY = Math.floor(Math.random() * (max - min + 1) ) + min;
 	//Draw a circle at location (x, y) with a diameter of 50.
-	circle(x+randInt, y, 50);
+	circle(x+randIntX, y+randIntY, 50);
 });
