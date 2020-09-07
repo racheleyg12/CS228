31b44c7b905ebb1ca813dae84d7c7088ade0d6af
racheleyg12 <rgoldma1@uvm.edu>
2020-09-07 17:39:58 -0400

    del 1 step45

---
+++ prepareToDraw.js
@@ -0,0 +1,5 @@
+//drawing canvas is ‘spread’ over the entire browser window
+//the first time creating a Javascript function
+function setup() {
+createCanvas(window.innerWidth,window.innerHeight);
+}
