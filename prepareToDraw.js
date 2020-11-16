4e31c25531c09e480d6333f66a1f02d8a2e0cbcf
racheleyg12 <rgoldma1@uvm.edu>
2020-11-16 00:33:19 -0500

    del 10 scaffolding 1

--- prepareToDraw.js
+++ prepareToDraw.js
@@ -6,6 +6,18 @@ var imgHandUp;
 var imgHandForward;
 var imgHandAway;
 
+var imgDigit0;
+var imgDigit1;
+var imgDigit2;
+var imgDigit3;
+var imgDigit4;
+var imgDigit5;
+var imgDigit6;
+var imgDigit7;
+var imgDigit8;
+var imgDigit9;
+
+
 //drawing canvas is ‘spread’ over the entire browser window
 function setup() {
 	createCanvas(window.innerWidth,window.innerHeight);
@@ -17,6 +29,14 @@ function setup() {
 	imgHandToward = loadImage('https://i.imgur.com/IIcWBZy.png');
 	imgHandAway = loadImage('https://i.imgur.com/LJxSQA6.png');
 
-	imgDigit1 = loadImage('https://i.imgur.com/KNEV2gF.png');
-	imgDigit2 = loadImage('https://i.imgur.com/kxEuArg.png');
+	imgDigit0 = loadImage('https://i.imgur.com/8AgabDB.png');
+	imgDigit1 = loadImage('https://i.imgur.com/xKUqQfv.png');
+	imgDigit2 = loadImage('https://i.imgur.com/gcokBQT.png');
+	imgDigit3 = loadImage('https://i.imgur.com/fJEpbO3.png');
+	imgDigit4 = loadImage('https://i.imgur.com/7NARzlD.png');
+	imgDigit5 = loadImage('https://i.imgur.com/rNQ7ZcB.png');
+	imgDigit6 = loadImage('https://i.imgur.com/YTjcmev.png');
+	imgDigit7 = loadImage('https://i.imgur.com/kHrlckl.png');
+	imgDigit8 = loadImage('https://i.imgur.com/xD05bGZ.png');
+	imgDigit9 = loadImage('https://i.imgur.com/9VdqrnK.png');
 }
